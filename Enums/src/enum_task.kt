2f3afb038ecbf06401ca7028d479ed32a8fa477a
Njood <nejood03@gmail.com>
2025-02-18 20:09:02 +0300

    step 4 and 5 are done and all the roles are listed

--- Enums/src/enum_task.kt
+++ Enums/src/enum_task.kt
@@ -1,16 +1,9 @@
-enum class UserRole(val accessLevel: Int)
-{ ADMIN(3), EDITOR(2), VIEWER(1) }
-
-
-fun checkUserAccess(role: UserRole, requiredLevel: Int): Boolean {
-    return role.accessLevel >= requiredLevel
+enum class UserRole(val accessLevel: Int) {
+    ADMIN(3), EDITOR(2), VIEWER(1)
 }
 
 fun main() {
-    val roles = listOf(UserRole.ADMIN, UserRole.EDITOR, UserRole.VIEWER)
-    val requiredLevel = 2
-
-    roles.forEach { role ->
-        println("${role.name} access: ${checkUserAccess(role, requiredLevel)}")
+    UserRole.values().forEach {
+        println("${it.name} , Access level: ${it.accessLevel}")
     }
 }
